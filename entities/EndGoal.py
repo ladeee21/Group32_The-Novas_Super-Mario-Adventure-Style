f9bf914edd44738360abe71595371141356185f9
andy-iio <andyguest12@gmail.com>
2025-03-31 17:32:08 -0400

    added flag image for end goal

--- entities/EndGoal.py
+++ entities/EndGoal.py
@@ -5,20 +5,20 @@ import pygame
 class Goal(pygame.sprite.Sprite):
     def __init__(self, screen, sprite_collection, x, y, level):
         pygame.sprite.Sprite.__init__(self)
-        self.sprite_collection = sprite_collection
-        self.image = self.sprite_collection.get("pipeR").image
-        self.rect = pygame.Rect(x * 32, y * 32, 32, 64)
         self.screen = screen
         self.x = x
         self.y = y
         self.level = level
         self.triggered = False
         self.type = "Goal" 
+
+        #loads the image of the flag
+        self.image = pygame.image.load("./img/flag.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (40, 90))
+        self.rect = self.image.get_rect() 
+        self.rect.topleft = ((x * 32) +20, y * 32) 
         
     def update(self, camera):
         if not self.triggered:
-            self.screen.blit(
-                self.image, 
-                ((self.x + camera.pos.x) * 32, self.y * 32)
-            )
+            self.screen.blit(self.image,((self.x + camera.pos.x) * 32, (self.y * 32)+70))
 
